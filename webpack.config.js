43af64ada0736460b58364b9f6dc8defa203421d
Charlize <charliezhou@outlook.com>
2024-03-28 17:17:32 -0700

    webpack dev env set up

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: "./client/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+    publicPath: "/build",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+      template: "index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              ["@babel/preset-env", { targets: "defaults" }],
+              ["@babel/preset-react"],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        //test:/\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname),
+    },
+    proxy: [
+      {
+        //context: ["/api"],
+        target: "http://localhost:3000",
+      },
+    ],
+  },
+};

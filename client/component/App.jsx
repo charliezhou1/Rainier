8b899b2f6e58559082fca2f0c89fffebdadce5ec
Charlize <charliezhou@outlook.com>
2024-04-01 20:04:07 -0700

    edit app for react router

--- client/component/App.jsx
+++ client/component/App.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import Navbar from "./Navbar.jsx";
 import ServiceList from "./ServiceList.jsx";
+import Signup from "./Signup.jsx";
+import Login from "./Login.jsx";
+import { BrowseRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
